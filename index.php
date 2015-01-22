8f41cf83851b6c198d5ad7bfae6918b82981f117
aleksandar013 <aleksandar.kresovic3@gmail.com>
2015-01-23 00:53:52 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>AddressBook</title>
+</head>
+<body>
+<form action="" method="post">
+<label for="ime">Ime:</label><br />
+<input type="text" name="ime" /><br />
+<label for="prezime">Prezime:</label><br />
+<input type="text" name="prezime" /><br />
+<label for="email">Email:</label><br />
+<input type="text" name="email" /><br />
+<input type="submit" name="submit" value="Unesi" />
+</form>
+</body>
+</html>
