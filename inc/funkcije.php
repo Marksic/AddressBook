8c7ef0197d119b7b557d573b27027030cf410e54
aleksandar013 <aleksandar.kresovic3@gmail.com>
2015-01-25 20:30:37 +0100

    input funcija

--- inc/funkcije.php
+++ inc/funkcije.php
@@ -1,5 +1,5 @@
 <?php
-// Naziv fajla: funkcije.php
+// Naziv fajla: input.php
 
 // Konstante za konekciju na bazu, naknadno treba popuniti
 DEFINE("HOST", ""); 
@@ -27,5 +27,16 @@ function clean($dbcon, $param) {
 	return $cleaned;
 }
 ////////////////////////
+//funkcija za ispisivanje input kontrola, sve input kontrole imaju labelu, osim kontrole tipa submit
+function input($type,$name,$name_label="",$value=""){
+	$output="";
+	if($type=='submit'){
+		$output.= "<input type='$type' name='$name' id='$name' value='$value' />";
+	}else {
+		$output.="<label for='$name'>$name_label</label>";
+		$output.= "<input type='$type' name='$name' id='$name' />";
+	}
+	return $output;
+}
 
 ?>
