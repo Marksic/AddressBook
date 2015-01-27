98f30508e2e0cca08279c50e2c724b39e7ca2b44
marksic <markovic.vr@gmail.com>
2015-01-27 21:43:51 +0100

    added frontend parts of adresbook project

---
+++ fronted/adresar.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>adresar</title>
+		<meta charset="utf-8">
+		<link href="style.css" type="text/css" rel="stylesheet" >
+	</head>
+	<body>
+		<div id="prvi">
+		<div id="wrapper">
+  				
+  				<header >	
+  					<nav>
+  						<ul>
+  							<li> <a href="#">Prijavi se</a> 
+  							</li>
+  							<li>
+  								<a href="#">Registruj se</a> 
+  							</li>
+  						</ul>
+  					</nav>		
+  				</header>
+  				<article>
+  					<img src="main3.jpg">
+  				</article>
+  				
+  		</div> <!-- kraj wrapper -->
+  		</div>
+	
+	<footer>
+		<div class="fotografija">
+			<div class="fotografija1"><img src="11.jpg"></div>
+			<div class="fotografija1"><img src="12.jpg"></div>
+			<div class="fotografija1"><img src="13.jpg"></div>
+		</div>
+		<div class="footer , footer_1"><a href="https://www.facebook.com/?ref=tn_tnmn"><img src="1.png"/></a></div>
+		<div class="footer"><a href="http://www.skype.com/en/"><img src="2.png"/></a></div>
+		<div class="footer"><a href="https://youtube.com/"><img src="3.png"/></a></div>
+		<div class="footer"><a href="https://plus.google.com/"><img src="4.png"/></a></div>
+		<div class="footer"><a href="https://twitter.com/"><img src="5.png"/></a></div>
+		<div class="footer"><a href="https://www.tumblr.com/"><img src="6.png"/></a></div>
+		<div class="footer"><a href="http://instagram.com/"><img src="7.png"/></a></div>
+		<div class="footer"><a href="https://www.gmail.com"><img src="8.png"/></a></div>
+		<div class="footer"><a href="https://www.blogger.com"><img src="9.png"/></a></div>
+		<div class="footer , footer_2"><a href="https://www.digg.com"><img src="10.png"/></a></div>
+		
+  	</footer>
+  	</body>
+</html>
